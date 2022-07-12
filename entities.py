0591d7d7183f64aea0cedf2f062a0cc5ab584805
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-07-13 00:02:05 +0300

    Change types of some fields

--- entities.py
+++ entities.py
@@ -17,13 +17,13 @@ class Distribution:
 @dataclass
 class Client:
 	id: int
-	mobile_number: int
-	mobile_operator_code: int
+	mobile_number: str
+	mobile_operator_code: str
 	tag: str
 	# timezone field should be chosen between:
 	# datetime.timezone or datetime.datetime.now(tz=pytz.timezone(tzlocal.get_localzone_name()))
 	# second one is equal to datetime.datetime.now(tz=pytz.timezone('Europe/Moscow'))
-	timezone: timezone  # or str if it will be looks like 'Europe/Moscow'
+	timezone: str  # or str if it will be looks like 'Europe/Moscow'
 
 
 @dataclass
