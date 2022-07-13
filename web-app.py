07fa180c235e78ec7b998f04a27dcbdcd995f74f
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-07-14 01:24:38 +0300

    1) Added "was_deleted" field which shows if row has been removed 2) Added some new routes: delete_clients, delete_client_by_pk, update_clients_attributes, update_client_attributes 3) Was made code review, have been fixed bugs

--- web-app.py
+++ web-app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, g, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from envparse import env
 from datetime import datetime, timedelta
 from json_validator.shema import DistributionSchema, ClientSchema, MessageSchema
 from marshmallow import ValidationError
 import pytz
 import secrets
+import json
 import logging
 
 
@@ -38,6 +40,7 @@ messages_schema = MessageSchema(many=True)
 datetime_format = '%Y-%m-%d %H:%M:%S'
 
 
+# need answer: how to move db models into another module?
 class Distribution(db.Model):
 	__tablename__ = 'distributions'
 
@@ -49,11 +52,13 @@ class Distribution(db.Model):
 	distr_text = db.Column(db.String, comment='message text')
 	client_filter = db.Column(db.String, nullable=True, comment='get some clients with filtering them by mobile operator code, tag or etc.')
 	distr_end_date = db.Column(db.DateTime, comment='date when distribution will be ended')
+	was_deleted = db.Column(db.Boolean, default=False, comment='Shows if this row has been removed')
 	message = db.relationship("Message", backref="distributions", lazy=True, uselist=False)
 
 	def __repr__(self):
 		return f'<Distribution: id: {self.id}, start_date: {self.distr_start_date.strftime(datetime_format)}, ' \
-			   f'text: "{self.distr_text}", client_filter: "{self.client_filter}", end_date: {self.distr_end_date.strftime(datetime_format)}>'
+			   f'text: "{self.distr_text}", client_filter: "{self.client_filter}", ' \
+			   f'end_date: {self.distr_end_date.strftime(datetime_format)}, was_deleted: {self.was_deleted}>'
 
 
 class Client(db.Model):
@@ -65,12 +70,14 @@ class Client(db.Model):
 	tag = db.Column(db.String, comment='free fillable field. Can be nullable')
 	# after API will be ready, user can give tz in "Europe/Moscow" like format. Table with values are there: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 	# or it can look like datetime.timezone('MSC') with offset from UTC-time.
-	timezone = db.Column(db.String(30), comment='it looks like "Europe/Moscow"')
+	timezone = db.Column(db.String(30), comment='it will be look like "Europe/Moscow"')
+	was_deleted = db.Column(db.Boolean, default=False, comment='Shows if this row has been removed')
 	message = db.relationship("Message", backref="clients", lazy=True)
 
 	def __repr__(self):
 		return f'<Client: id: {self.id}, mobile_number: "{self.mobile_number}", ' \
-			   f'operator_code: "{self.mobile_operator_code}", tag: "{self.tag}", timezone: "{self.timezone}">'
+			   f'operator_code: "{self.mobile_operator_code}", tag: "{self.tag}", timezone: "{self.timezone}, ' \
+			   f'was_deleted: {self.was_deleted}">'
 
 
 class Message(db.Model):
@@ -78,15 +85,97 @@ class Message(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	send_date = db.Column(db.DateTime, default=datetime.now(), comment='date when message was send')
-	sending_status = db.Column(db.Boolean, default=True)
+	sending_status = db.Column(db.Boolean, default=False)
 	distribution_id = db.Column(db.Integer, db.ForeignKey('distributions.id'), comment='distribution id, where message was sended')
 	# distribution = db.relationship("Distribution", backref="message")
 	client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), comment='client id whose was send message')
 	# client = db.relationship('Client', backref='messages', lazy=True)
+	was_deleted = db.Column(db.Boolean, default=False, comment='Shows if this row has been removed')
 
 	def __repr__(self):
 		return f'<Message: id: {self.id}, send_date: {self.send_date.strftime(datetime_format)}, ' \
-			   f'sending_status: {self.sending_status}, distribution.id: {self.distribution_id}, client.id: {self.client_id}>'
+			   f'sending_status: {self.sending_status}, distribution.id: {self.distribution_id}, ' \
+			   f'client.id: {self.client_id}, was_deleted: {self.was_deleted}>'
+
+
+def dynamic_update(objects, attrs):
+	for obj in objects:
+		for k, v in attrs.items():
+			if hasattr(obj, k):
+				setattr(obj, k, v)
+				db.session.commit()
+	return objects
+
+
+@app.route('/api/v1/client/delete', methods=['get'])
+def delete_clients():
+	args = request.args
+	if not args:
+		return {"message": "No filter arguments provided"}, 400
+	try:
+		Client.query.filter_by(**args).update(dict(was_deleted=True))
+	except InvalidRequestError as err:
+		return {"messages": err.args[0]}
+	clients = Client.query.filter_by(**args).all()
+	db.session.commit()
+	result = clients_schema.dump(clients)
+	return {"message": "Successful delete", "clients": result}
+
+
+@app.route('/api/v1/client/delete/<int:pk>')
+def delete_client_by_pk(pk):
+	try:
+		Client.query.filter_by(id=pk).update(dict(was_deleted=True))
+	except InvalidRequestError as err:
+		return {"messages": err.args[0]}
+	client = Client.query.filter_by(id=pk).first()
+	result = client_schema.dump(client)
+	db.session.commit()
+	return {"message": "Successful delete", "client": result}
+
+
+@app.route('/api/v1/client/update', methods=['get', 'post'])
+def update_clients_attributes():
+	if request.method == 'POST':
+		args = dict(request.args)
+		json_data = request.get_json()
+		if args.get('was_deleted'):
+			args['was_deleted'] = False if args['was_deleted'].lower() == 'false' else True
+		if json_data.get('was_deleted'):
+			json_data['was_deleted'] = False if json_data['was_deleted'].lower() == 'false' else True
+		try:
+			# result = db.session.query(User.money).with_for_update().filter_by(id=userid).first()
+			# 1
+			# nums = Client.query.filter_by(**args).with_for_update(of=Client).update(json_data)
+			# clients = Client.query.filter_by(**args).all()
+			# 2
+			clients = Client.query.filter_by(**args).all()
+			updated_clients = dynamic_update(clients, json_data)
+		except InvalidRequestError as err:
+			return {"messages": err.args[0]}
+		db.session.commit()
+		result = clients_schema.dump(updated_clients, many=True)
+		return {"messages": "Successful update", "client": result}
+	else:
+		return {"message": "GET method is not implemented"}, 405
+
+
+@app.route('/api/v1/client/update/<int:pk>', methods=['get', 'post'])
+def update_client_attributes(pk):
+	if request.method == 'POST':
+		json_data = request.get_json()
+		if json_data.get('was_deleted'):
+			json_data['was_deleted'] = False if json_data['was_deleted'].lower() == 'false' else True
+		try:
+			Client.query.filter_by(id=pk).update(json_data)
+			client = Client.query.filter_by(id=pk).first()
+		except InvalidRequestError as err:
+			return {"messages": err.args[0]}
+		db.session.commit()
+		result = client_schema.dump(client)
+		return {"messages": "Successful update", "client": result}
+	else:
+		return {"message": "GET method is not implemented"}, 405
 
 
 @app.route('/api/v1/client/', methods=['get', 'post'])
@@ -106,12 +195,14 @@ def create_client():
 			client = Client(**data)
 			db.session.add(client)
 			db.session.commit()
-		result = client_schema.dump(client)
-		return {"message": "Created new client.", "client": result}
+			result = client_schema.dump(client)
+			return {"message": "Created new client", "client": result}
+		else:
+			# return existed client
+			result = client_schema.dump(client)
+			return {"message": "Client already exists", "client": result}
 	else:
-		clients = Client.query.all()
-		result = clients_schema.dump(clients, many=True)
-		return {"messages": result}
+		return {"message": "GET method is not implemented"}, 405
 
 
 if __name__ == "__main__":
