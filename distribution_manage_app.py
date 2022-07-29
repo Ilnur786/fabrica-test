aac966c5a075105d9a411bc683d775b1884c9cc0
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-07-29 14:11:20 +0300

    Deleted unnecessary comments

--- distribution_manage_app.py
+++ distribution_manage_app.py
@@ -24,30 +24,4 @@ Base.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
-	# db.create_all()  # it should be here
-
-	# d = Distribution(start_date=datetime.now(), text='hello', client_filter='mts',
-	# 				 end_date=datetime.now() + timedelta(days=1))
-	# c = Client(mobile_number="79177456985", mobile_operator_code="917", tag='mts', timezone='Europe/Moscow')
-	# db.session.add_all([d, c])
-	# m = Message(distribution_id=4)
-	# db.session.add(m)
-	# db.session.commit()
-	# print("A distr entity was added to the table")
-
-	# # read the data
-	# d1 = app.session.query(Distribution).filter_by(id=1).first()
-	# print(d1)
-	# m1 = app.session.query(Message).filter_by(distribution_id=d1.id).first()
-	# print(m1)
-	# print('=' * 150)
-	# rows = app.session.query(Distribution).all()
-	# print(*rows, sep='\n')
-	# print('=' * 150)
-	# clients = app.session.query(Distribution).all()
-	# print(*clients, sep='\n')
-	# print('=' * 150)
-	# messages = app.session.query(Message).all()
-	# print(*messages, sep='\n')
-
 	app.run(debug=True)
