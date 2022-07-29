cc179c12cf4aa6a536ce76032c58e4858f93223b
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-07-29 14:15:17 +0300

    Added comment

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,5 @@
-# Common pattern name is MVC (model-view-control), and here is controllers
-# In python (clearly in Django) that is MTV (model-template-view), and here is views
+# Common pattern name is MVC (model-view-control), that why package name is "controllers"
+# In python (clearly in Django) that is MTV (model-template-view), and here is "views"
 from controllers.client import app_client
 from controllers.distribution import app_distribution
 from controllers.message import app_messsage
