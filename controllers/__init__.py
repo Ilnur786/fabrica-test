7435da8401e9339a028f3466f0bde9c7b18f37ad
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-07-29 12:34:19 +0300

    Added blueprints imports

---
+++ controllers/__init__.py
@@ -0,0 +1,6 @@
+# Common pattern name is MVC (model-view-control), and here is controllers
+# In python (clearly in Django) that is MTV (model-template-view), and here is views
+from controllers.client import app_client
+from controllers.distribution import app_distribution
+from controllers.message import app_messsage
+from controllers.statistic import app_statistic
