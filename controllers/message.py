e712304fc6f818286ec1f3d0f1c60159c015d424
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-07-29 12:37:40 +0300

    Routes from manage app have been separate to each module in controllers package

---
+++ controllers/message.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, _app_ctx_stack
+from sqlalchemy.orm import scoped_session
+# CURRENT PROJECT MODULES
+from db_api import Message
+from db_api import SessionLocal
+from json_validator import MessageSchema
+
+
+app_messsage = Blueprint('app_messsage', __name__)
+app_messsage.session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack)
+
+# CREATE MARSHMALLOW SCHEMAS INSTANCES
+message_schema = MessageSchema()
+messages_schema = MessageSchema(many=True)
+
+
+# MESSAGES ROUTES SECTION
+
+@app_messsage.route('/api/v1/message/', methods=['get'])
+def get_all_messages():
+	messages = app_messsage.session.query(Message).all()
+	result = messages_schema.dump(messages)
+	return {"message": "All messages", "messages": result}
+
+
+@app_messsage.route('/api/v1/message/all', methods=['get'])
+def get_all_messages_another_route():
+	messages = app_messsage.session.query(Message).all()
+	result = messages_schema.dump(messages)
+	return {"message": "All messages", "messages": result}
+
+
+@app_messsage.route('/api/v1/message/<int:pk>')
+def get_messages_include_deleted(pk):
+	messages = app_messsage.session.query(Message).filter_by(id=pk).all()
+	result = messages_schema.dump(messages)
+	return {"message": "Matched message", "messages": result}
