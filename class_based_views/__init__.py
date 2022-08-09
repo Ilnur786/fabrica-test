ed79dbeceb8bba557741db9063caa3849937db99
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-08-09 21:29:29 +0300

    Added statistic namespace

--- class_based_views/__init__.py
+++ class_based_views/__init__.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 from flask_restx import Api
 from class_based_views.client import ns as client_ns
 from class_based_views.distribution import ns as distribution_ns
+from class_based_views.statistic import ns as statistic_ns
+from class_based_views.message import ns as message_ns
 
 doc_blueprint = Blueprint('documented_api', __name__)
 
@@ -12,3 +14,8 @@ api_extension = Api(doc_blueprint, doc='/docs/', version='1.0', title='Distribut
 
 api_extension.add_namespace(client_ns, '/api/v1')
 api_extension.add_namespace(distribution_ns, '/api/v1')
+# api_extension.add_namespace(message_ns, '/api/v1')  # почему то message_ns и statistic_ns перетирают друг друга
+api_extension.add_namespace(statistic_ns, '/api/v1')
+
+
+
