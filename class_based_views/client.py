ab88bad8c9e2413e837078202e69f7899392ce5e
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-08-09 19:54:29 +0300

    Refactoring: 1) "marshal_with" decor was changed on "response" 2) Changed some logic 3) Bugs have been fixed

--- class_based_views/client.py
+++ class_based_views/client.py
@@ -28,18 +28,18 @@ def default_err_handler(error):
 ns = api.namespace('Client', description='Client related endpoints')
 
 # CLIENT METHOD PARSER
-parser_without_deleted = api.parser()
-parser_without_deleted.add_argument('id', type=int)
-parser_without_deleted.add_argument('mobile_number', type=str)
-parser_without_deleted.add_argument('mobile_operator_code', type=str)
-parser_without_deleted.add_argument('tag', type=str)
-parser_without_deleted.add_argument('timezone', type=str)
+parser_client = api.parser()
+parser_client.add_argument('id', type=int)
+parser_client.add_argument('mobile_number', type=str)
+parser_client.add_argument('mobile_operator_code', type=str)
+parser_client.add_argument('tag', type=str)
+parser_client.add_argument('timezone', type=str)
 
 
 # CLIENT MODELS
 client_model = ns.model('Client', {
     'id': fields.Integer(readonly=True, description='Client unique identifier'),
-    'mobile_number': fields.String(required=True, description='Client mobile number'),
+    'mobile_number': fields.String(required=True, description='Client mobile number', example='79178542569'),
     'mobile_operator_code': fields.String(description='Client mobile operator code'),
     'tag': fields.String(required=True, description='Client filter tag'),
     'timezone': fields.String(description='Client tz. Default value is local tz'),
@@ -71,8 +71,9 @@ class ClientView(Resource):
     # @NS.DOC NEED TO ASSIGN EXPECTED QUERY PARAMS IN SWAGGER DOC.
     # @NS.EXPECT MAKE THIS WORK TOO.
     # @ns.doc(params={'id': '', 'mobile_number': '', 'mobile_operator_code': '', 'tag': '', 'timezone': '', 'was_deleteted': ''})
-    @ns.expect(parser_without_deleted, validate=False)
-    @ns.marshal_list_with(clients_model_response)
+    @ns.expect(parser_client, validate=False)
+    # @ns.marshal_list_with(clients_model_response)
+    @ns.response(200, model=clients_model_response, description='Matched clients')
     @ns.response(422, 'Error message')
     def get(self):
         """ Get clients filtered by query params """
@@ -82,6 +83,8 @@ class ClientView(Resource):
                 clients = app_client.session.query(Client).filter_by(was_deleted=False).all()
             except InvalidRequestError as err:
                 return {"messages": err.args[0]}, 422
+            except Exception:
+                return {"message": "External Error"}
             result = clients_schema.dump(clients)
             return {"message": "All clients, exclude deleted", "clients": result}
         else:
@@ -89,11 +92,14 @@ class ClientView(Resource):
                 clients = app_client.session.query(Client).filter_by(**http_args, was_deleted=False).all()
             except InvalidRequestError as err:
                 return {"messages": err.args[0]}, 422
+            except Exception:
+                return {"message": "External Error"}
             result = clients_schema.dump(clients)
             return {"message": "Matched clients", "clients": result}
 
     @ns.expect(client_model, validate=False)
-    @ns.marshal_with(client_model_response)
+    # @ns.marshal_with(client_model_response)
+    @ns.response(200, model=client_model_response, description='Created new client')
     @ns.response(422, 'Error message')
     @data_provided_validator
     def post(self):
@@ -104,62 +110,70 @@ class ClientView(Resource):
             data = client_schema.load(json_data)
         except ValidationError as err:
             return err.messages, 422
-        client = app_client.session.query(Client).filter_by(mobile_number=data['mobile_number']).first()
-        if client is None:
-            # Create a new client
-            client = Client(**data)
-            app_client.session.add(client)
-            app_client.session.commit()
-            result = client_schema.dump(client)
-            return {"message": "Created new client", "client": result}
+        try:
+            client = app_client.session.query(Client).filter_by(mobile_number=data['mobile_number']).first()
+        except Exception:
+            return {"messages": 'External Error'}, 422
         else:
-            # return existed client
-            result = client_schema.dump(client)
-            return {"message": "Client already exists", "client": result}
+            if client is None:
+                # Create a new client
+                client = Client(**data)
+                app_client.session.add(client)
+                app_client.session.commit()
+                result = client_schema.dump(client)
+                return {"message": "Created new client", "client": result}
+            else:
+                # return existed client
+                result = client_schema.dump(client)
+                return {"message": "Client already exists", "client": result}
 
 
 @ns.route('/client/<int:pk>')
 class ClientIdView(Resource):
     @ns.doc('get_client')
-    @ns.marshal_with(client_model_response)
+    # @ns.marshal_with(client_model_response)
+    @ns.response(200, model=client_model_response, description='Requested client')
     @ns.response(422, 'Error message')
+    @ns.response(404, 'Not Found')
     def get(self, pk):
         """ Get client via id """
-        try:
-            client = app_client.session.query(Client).filter_by(id=pk).first()
-        except InvalidRequestError as err:
-            return {"messages": err.args[0]}, 422
+        client = app_client.session.query(Client).filter_by(id=pk).first()
+        if client is None:
+            return {"message": "Not found"}, 404
         result = client_schema.dump(client)
         return {"message": "Requested client", "client": result}
 
     @ns.doc('update_client')
     @ns.expect(client_model_for_update)
-    @ns.marshal_with(client_model_response)
+    # @ns.marshal_with(client_model_response)
+    @ns.response(200, model=client_model_response, description='Successful update')
     @ns.response(422, 'Error message')
+    @ns.response(404, 'Not Found')
     @data_provided_validator
     def put(self, pk):
         """ Update client attributes via id """
         json_data = request.get_json()
         client = app_client.session.query(Client).filter_by(id=pk).first()
+        if client is None:
+            return {"message": "Not found"}, 404
         updated_client = dynamic_update(client, json_data)
         try:
             app_client.session.commit()
-        except Exception as err:
-            return {"messages": err.args[0]}, 422
+        except Exception:
+            return {"messages": 'External Error'}, 422
         result = client_schema.dump(updated_client)
         return {"message": "Successful update", "client": result}
 
     @ns.doc('delete_client')
     @ns.response(422, 'Error message')
-    @ns.marshal_with(client_model_response)
+    # @ns.marshal_with(client_model_response)
+    @ns.response(200, model=client_model_response, description='Successful delete')
+    @ns.response(404, 'Not Found')
     def delete(self, pk):
         """ Change 'was_deleted' status on True """
-        try:
-            client = app_client.session.query(Client).filter_by(id=pk).first()
-        except InvalidRequestError as err:
-            return {"messages": err.args[0]}
+        client = app_client.session.query(Client).filter_by(id=pk).first()
         if not client:
-            return {"message": "No one client with given id"}
+            return {"message": "Not Found"}
         updated_client = dynamic_update(client, dict(was_deleted=True))
         app_client.session.commit()
         result = client_schema.dump(updated_client)
@@ -169,13 +183,10 @@ class ClientIdView(Resource):
 @ns.route('/client/all')
 class ClientAllView(Resource):
     @ns.doc('get_client_list_include_deleted')
-    @ns.marshal_with(clients_model_response)
-    @ns.response(200, 'All clients, include deleted')
+    # @ns.marshal_with(clients_model_response)
+    @ns.response(200, model=clients_model_response, description='All clients, include deleted')
     def get(self):
         """ Get all clients, include deleted """
-        try:
-            clients = app_client.session.query(Client).all()
-        except InvalidRequestError as err:
-            return {"messages": err.args[0]}, 422
+        clients = app_client.session.query(Client).all()
         result = clients_schema.dump(clients)
         return {"message": "All clients, include deleted", "clients": result}
